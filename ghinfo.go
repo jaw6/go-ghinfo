8cebc285f964cfc5f0e63a61f50ac33a7cf0a392
Joshua Wehner <joshua.wehner@github.com>
2015-05-27 11:07:17 -0500

    Barebones repoDetails

--- ghinfo.go
+++ ghinfo.go
@@ -3,13 +3,36 @@ package main
 import (
   "flag"
   "fmt"
+  "strings"
   "github.com/octokit/go-octokit/octokit"
 )
 
 var userFlag = flag.String("user", "", "User name")
+var repoFlag = flag.String("repo", "", "Repo name or URL")
 
 func init() {
   flag.StringVar(userFlag, "u", "", "User name")
+  flag.StringVar(repoFlag, "r", "", "Repo name or URL")
+}
+
+func makeHeader(head string) (header string) {
+  hr   := ""
+  for i:=0; i<len(head); i++ { hr += "=" }
+  header = fmt.Sprintf(head + "\n" + hr + "\n")
+  return
+}
+
+func getRepo(input string) (owner, repo string) {
+  parts := strings.Split(input, "/")
+  l := len(parts)
+  // repoName = fmt.Sprintf("%s/%s", parts[l-2], parts[l-1])
+  owner, repo = parts[l-2], parts[l-1]
+  return
+}
+
+func repoDetails(repoInput string) {
+  owner, repo := getRepo(repoInput)
+  fmt.Println(makeHeader(fmt.Sprintf("Details for repository %v/%v", owner, repo)))
 }
 
 func usageDetails() {
@@ -17,10 +40,7 @@ func usageDetails() {
 }
 
 func userDetails(userName string) {
-  head := fmt.Sprintf("Details for GitHub user %v", userName)
-  hr   := ""
-  for i:=0; i<len(head); i++ { hr += "=" }
-  fmt.Printf(head + "\n" + hr + "\n\n")
+  fmt.Println(makeHeader(fmt.Sprintf("Details for GitHub user %v", userName)))
 
   client := octokit.NewClient(nil)
 
@@ -51,6 +71,7 @@ func main() {
   // fmt.Println(*userFlag)
   switch {
     case *userFlag != "": userDetails(*userFlag)
+    case *repoFlag != "": repoDetails(*repoFlag)
     default: usageDetails()
   }
 }
